707ca7c8add6fcaeb9a700f4913d4ca3239f8da3
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-12-01 21:15:09 +0200

    feat: Functional Pathfinding v1.0.

--- src/com/rat/utils/NMaze.java
+++ src/com/rat/utils/NMaze.java
@@ -11,7 +11,8 @@ public class NMaze {
     final List<ArrayList<Integer>> physicalProgress;
     Semaphore availablePaths;
     AtomicInteger solutions;
-    ConcurrentLinkedQueue<ArrayList<Integer>> mazeFrontier;
+    ConcurrentHashMap<Integer, ArrayList<Integer>> mazeFrontier;
+    ConcurrentHashMap<String, Boolean> mazeSkip;
     ThreadPoolExecutor miceManger;
     int stampId;
     ArrayList<ArrayList<ArrayList<Integer>>> solutionPaths;
@@ -27,8 +28,9 @@ public class NMaze {
                         .collect(Collectors.toCollection(ArrayList::new)))
                 .collect(Collectors.toCollection(ArrayList::new));
         this.solutions = new AtomicInteger(0);
-        this.mazeFrontier = new ConcurrentLinkedQueue<>();
-        this.mazeFrontier.add(new ArrayList<Integer>(List.of(0, 0, 0)));
+        this.mazeFrontier = new ConcurrentHashMap<>();
+        this.mazeFrontier.put(0, new ArrayList<Integer>(List.of(0, 0)));
+        this.mazeSkip = new ConcurrentHashMap<>();
         this.availablePaths = new Semaphore(1);
         this.solutionPaths = new ArrayList<>(30);
         this.stampId = 0;
@@ -37,7 +39,7 @@ public class NMaze {
     synchronized public int solve() throws InterruptedException {
         this.miceManger = new ThreadPoolExecutor(8, 16, 200, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
         this.miceManger.submit(new Rat(this.stampId, this));
-        this.stampId+=1;
+        this.stampId += 1;
 
         while (miceManger.getActiveCount() > 0) {
             Thread.sleep(5000);
@@ -47,36 +49,35 @@ public class NMaze {
     }
 
     public void getSolutionPaths() {
-        this.solutionPaths.stream().forEach((ArrayList<ArrayList<Integer>> mouseList)->{
+        this.solutionPaths.stream().forEach((ArrayList<ArrayList<Integer>> mouseList) -> {
             System.out.print(mouseList.getFirst().getLast() + ": ");
-           mouseList.stream().forEach((ArrayList<Integer> mouseStep)->{
-            System.out.print("(" +mouseStep.getFirst() + ", " + mouseStep.get(1) + ") -> ");
-           });
-        System.out.println("X");
+            mouseList.stream().forEach((ArrayList<Integer> mouseStep) -> {
+                System.out.print("(" + mouseStep.getFirst() + ", " + mouseStep.get(1) + ") -> ");
+            });
+            System.out.println("X");
         });
     }
 
 
-    public ArrayList<ArrayList<Integer>> peekFrom(int x, int y, int callerId, boolean allowRevisitWhenDead) {
-        synchronized (this.physicalProgress) {
+    public ArrayList<ArrayList<Integer>> peekFrom(int x, int y) {
             ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-            result.add(new ArrayList<>(List.of(0, 0, callerId)));
-            result.add(new ArrayList<>(List.of(0, 0, callerId)));
-            int rightState = (x + 1 < this.size) ? this.physicalProgress.get(y).get(x + 1) : 0;
-            int downState = (y + 1 < this.size) ? this.physicalProgress.get(y + 1).get(x) : 0;
+            result.add(new ArrayList<>(List.of(0, 0)));
+            result.add(new ArrayList<>(List.of(0, 0)));
+            String rightKey = (x + 1) + "," + y;
+            String downKey = x + "," + (y + 1);
+            boolean rightIsEffectiveOpen = x + 1 < this.size && this.physicalSpace[y][x+1] == 1 && this.mazeSkip.get(rightKey) == null;
+            boolean downIsEffectiveOpen = y + 1 < this.size && this.physicalSpace[y+1][x] == 1 && this.mazeSkip.get(downKey) == null;
 //        boolean rightIsEffectiveOpen = (rightState == 1 || ((rightState == 2 && downState == 0) && allowRevisitWhenDead));
 //        boolean downIsEffectiveOpen = (downState == 1 || ((downState == 2 && rightState == 0 ) && allowRevisitWhenDead));
-            boolean rightIsEffectiveOpen = (rightState == 1);
-            boolean downIsEffectiveOpen = (downState == 1);
+//            boolean rightIsEffectiveOpen = (rightState == 1);
+//            boolean downIsEffectiveOpen = (downState == 1);
             if (rightIsEffectiveOpen) {
-                result.set(0, new ArrayList<>(List.of(x + 1, y, callerId)));
+                result.set(0, new ArrayList<>(List.of(x + 1, y)));
             }
             if (downIsEffectiveOpen) {
-                result.set(1, new ArrayList<>(List.of(x, y + 1, callerId)));
+                result.set(1, new ArrayList<>(List.of(x, y + 1)));
             }
-
             return result;
-        }
     }
 
 
@@ -84,7 +85,7 @@ public class NMaze {
         this.availablePaths.release();
 //        ArrayList<Integer> newStart = this.mazeFrontier.poll();
         this.miceManger.submit(new Rat(this.stampId, this));
-        this.stampId+=1;
+        this.stampId += 1;
     }
 
 //    public boolean isDeadEnd(int x, int y) {
--- src/com/rat/utils/Rat.java
+++ src/com/rat/utils/Rat.java
@@ -12,58 +12,39 @@ public class Rat implements Callable<int[]> {
     NMaze maze;
 
     public Rat(int id, NMaze maze) {
-//        this.startX = x;
-//        this.currentX = x;
-//        this.startY = y;
-//        this.currentY = y;
         this.mouseId = id;
         this.maze = maze;
     }
 
     @Override
     public int[] call() throws InterruptedException {
-        while (!maze.mazeFrontier.isEmpty()) {
+        while (this.maze.mazeFrontier.containsKey(this.mouseId)) {
             this.maze.availablePaths.acquire();
-            if (this.maze.mazeFrontier.peek().get(2) != this.mouseId) {
-                Thread.sleep(5);
-            }
-            ArrayList<Integer> current = maze.mazeFrontier.poll();
-            synchronized (this.maze.physicalProgress) {
-                this.maze.physicalProgress.get(current.get(1)).set(current.get(0), 2);
-//                System.out.println("  0 1 2 3 4 5 6 7");
-//                for (int i = 0; i < 8; i++) {
-//                    System.out.print(i + " ");
-//                    for (int j = 0; j < 8; j++) {
-//                        if (i == current.get(1) && j == current.get(0)) {
-//                            System.out.print(this.maze.physicalProgress.get(i).get(j) + "<");
-//                        } else {
-//                            System.out.print(this.maze.physicalProgress.get(i).get(j) + " ");
-//                        }
-//                    }
-//                        System.out.println();
-//                }
-            }
-            ArrayList<ArrayList<Integer>> peekResult = maze.peekFrom(current.get(0), current.get(1), this.mouseId, false);
-//            System.out.println("I'm mouse:" + this.mouseId + " and I'm at (" + current.get(0) + ", " + current.get(1) + ").");
+            ArrayList<Integer> current = this.maze.mazeFrontier.get(this.mouseId);
+            this.maze.mazeFrontier.remove(this.mouseId);
+            this.currentX = current.get(0);
+            this.currentY = current.get(1);
+            String key = current.get(0) + "," + current.get(1);
+            this.maze.mazeSkip.put(key, true);
             try {
                 this.maze.solutionPaths.get(this.mouseId).add(current);
-            } catch (Exception e){
+            } catch (Exception e) {
                 this.maze.solutionPaths.add(this.mouseId, new ArrayList<>());
                 this.maze.solutionPaths.get(this.mouseId).add(current);
             }
-            if (peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0, this.mouseId))) && peekResult.get(1).equals(new ArrayList<Integer>(List.of(0, 0, this.mouseId)))) {
+            ArrayList<ArrayList<Integer>> peekResult = maze.peekFrom(current.get(0), current.get(1));
+//            System.out.println("I'm mouse:" + this.mouseId + " and I'm at (" + current.get(0) + ", " + current.get(1) + ").");
+            if (peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0))) && peekResult.get(1).equals(new ArrayList<Integer>(List.of(0, 0)))) {
 //                System.out.println("Dead-end at (" + current.get(0) + ", " + current.get(1) + ").");
                 return new int[]{0, 0};
-            } else if (!peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0, this.mouseId))) && !peekResult.get(1).equals(new ArrayList<Integer>(List.of(0, 0, this.mouseId)))) {
+            } else if (!peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0))) && !peekResult.get(1).equals(new ArrayList<Integer>(List.of(0, 0)))) {
 //                System.out.println("Fork at (" + current.get(0) + ", " + current.get(1) + ").");
-                maze.mazeFrontier.add(peekResult.get(0));
-                peekResult.get(1).set(2, this.maze.stampId);
-                maze.mazeFrontier.add(peekResult.get(1));
+                maze.mazeFrontier.put(this.mouseId, peekResult.get(0));
+                maze.mazeFrontier.put(this.maze.stampId, peekResult.get(1));
                 this.maze.availablePaths.release();
                 this.maze.signalFork();
             } else {
-                maze.mazeFrontier.add(!peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0, this.mouseId))) ? peekResult.get(0) : peekResult.get(1));
-                System.out.println();
+                maze.mazeFrontier.put(this.mouseId, !peekResult.get(0).equals(new ArrayList<Integer>(List.of(0, 0))) ? peekResult.get(0) : peekResult.get(1));
                 this.maze.availablePaths.release();
             }
         }
