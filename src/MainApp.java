aa20f0054af4046253d253fd5628d8861881a31a
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-12-05 01:55:07 +0200

    feat: start w/ FX.

---
+++ src/MainApp.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/MainApp.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root,  900, 600);
+        stage.setTitle("Mouse Maze");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
